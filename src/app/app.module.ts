2507a684f141523f0e8e86fbcf50961d19f1f500
Hugo Anjos <hugo.h.anjos@hotmail.com>
2020-11-22 00:08:20 -0300

    adicionando componentes restantes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,21 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { TestComponent } from './components/test/test.component';
 import { AboutComponent } from './components/about/about.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { AnswersComponent } from './components/answers/answers.component';
+import { SequenceComponent } from './components/sequence/sequence.component';
+import { SocialComponent } from './components/social/social.component';
+import { VerticalAdComponent } from './components/vertical-ad/vertical-ad.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     TestComponent,
-    AboutComponent
+    AboutComponent,
+    AnswersComponent,
+    SequenceComponent,
+    SocialComponent,
+    VerticalAdComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/answers/answers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-answers',
+  templateUrl: './answers.component.html',
+  styleUrls: ['./answers.component.sass']
+})
+export class AnswersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
