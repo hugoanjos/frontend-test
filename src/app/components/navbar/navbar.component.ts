a41b456388034e5e72ee33421474431dce5acbe5
Hugo Anjos <hugo.h.anjos@hotmail.com>
2020-11-23 16:55:08 -0300

    logo seguindo cor do tema

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -8,16 +8,21 @@ import { ThemeService } from 'src/app/theme/theme.service';
 })
 export class NavbarComponent implements OnInit {
 
+  isDarkTheme: boolean;
+
   constructor(private themeService: ThemeService) {}
 
   ngOnInit(): void {
+    this.isDarkTheme = this.themeService.isDarkTheme();
   }
 
   toggleTheme() {
     if (this.themeService.isDarkTheme()) {
       this.themeService.setLightTheme();
+      this.isDarkTheme = false;
     } else {
       this.themeService.setDarkTheme();
+      this.isDarkTheme = true;
     }
   }
 
